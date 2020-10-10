4019366cd30957ec693921c67375fbc5f0dbddb7
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-09 22:52:17 -0700

    fixed error - links not showing up for company pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SearchForm from './components/SearchForm';
 import Navigation from './components/Navigation';
 import Profiles from './components/Profiles';

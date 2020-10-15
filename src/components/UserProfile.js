68c3d1257215df5f194bdfeb58009bea24657069
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-15 10:17:07 -0700

    initialized userprofile styling

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext, WatchListContext } from './../App';
 import CompanyCard from './CompanyCard';
+import './../App.css'
+import './../styles/UserProfile.css';
 
 const UserProfile = () => {
 
@@ -32,8 +34,10 @@ const UserProfile = () => {
   return (
     <React.Fragment>
       <div className='userprofile'>
-        <div className='userprofile__name'>{user.name}</div>
-        <div className='userprofile__image'><img src={user.imageUrl} alt='user' /></div>
+        <div className='userprofile__user-info'>
+          <div className='userprofile__image'><img src={user.imageUrl} alt='user' /></div>
+          <div className='userprofile__name'>My Collection ({collection.length} stocks)</div>
+        </div>
         <div className='profile__container'>
           {
             collection.map((companyProfile, i) => {

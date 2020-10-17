9ad31ce56ea8949815ade6e5be0ba5625d71ffc9
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-17 01:12:09 -0700

    removed dummy variables

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navigation from './components/Navigation';
 import SearchResults from './components/SearchResults';
 import Company from './components/Company';
 import UserProfile from './components/UserProfile.js';
-import dummyState from './dummy';
 import './App.css';
 
 export const UserContext = React.createContext(null);
@@ -16,12 +15,12 @@ export const WatchListContext = React.createContext(null);
 const App = () => {
 
   // An array of retrieved companies that matches user's query
-  const [searchResults, setSearchResults] = useState(dummyState); // use [];
+  const [searchResults, setSearchResults] = useState([]); // use [];
 
   // Query options and value of the index of the first result for scraping Finviz
   const [queryOptions, setQueryOptions] = useState('');
   const [initialValue, setInitialValue] = useState(1);
-  const [totalResultCount, setTotalResultCount] = useState(225);
+  const [totalResultCount, setTotalResultCount] = useState(0);
 
   // Object containing authenticated user information
   const [user, setUser] = useState({});

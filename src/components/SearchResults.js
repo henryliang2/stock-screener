33ef5a6053458fa80c4fac84ebda952cb7972104
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-17 15:24:27 -0700

    added progress bar when searching

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,8 +1,9 @@
 import React, { useContext, useRef } from 'react';
 import { SearchResultContext } from './../App'
 import CompanyCard from './CompanyCard';
+import { withStyles } from "@material-ui/core/styles";
+import { LinearProgress } from '@material-ui/core';
 import './../App.css';
-
 
 const SearchResults = (props) => {
 
@@ -10,10 +11,11 @@ const SearchResults = (props) => {
 
   const { searchResults, setSearchResults } = useContext(SearchResultContext);
 
-  console.log(props.initialValue, props.totalResultCount)
-
   return (
     <React.Fragment>
+      { // If loading (ie. totalResultCount set to -1) show loading bar
+        (props.totalResultCount === -2 ) && <StyledLinearProgress />
+      }
       
       <div className='profile__resultcount' ref={ resultCountRef }>
         { // If < 20 results, display number of results on page
@@ -26,9 +28,6 @@ const SearchResults = (props) => {
         }
         { // If no results (totalResultCount set to -1), display 'no results found'
           (props.totalResultCount === -1 ) && 'No Results Found =(' 
-        }
-        { // If no results (totalResultCount set to -1), display 'no results found'
-          (props.totalResultCount === -2 ) && 'Searching ...' 
         }
       </div>
 
@@ -59,5 +58,14 @@ const SearchResults = (props) => {
     </React.Fragment>
   );
 }
+
+const StyledLinearProgress = withStyles({
+  colorPrimary: {
+    backgroundColor: "#111b30"
+  },
+  barColorPrimary: {
+    backgroundColor: "#5eccc3"
+  }
+})(LinearProgress);
 
 export default SearchResults;

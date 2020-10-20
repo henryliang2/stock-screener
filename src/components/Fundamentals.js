d2e4cf6d2e0b0f4c747c4843c5a8523db212ccc2
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-20 15:21:42 -0700

    moved fundamental explanations for seperate variable file

--- src/components/Fundamentals.js
+++ src/components/Fundamentals.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import { Tooltip } from '@material-ui/core';
+import { indicatorDescriptions } from './../variables/indicatorDescriptions';
 
 const Fundamentals = (props) => {
 
@@ -25,26 +26,38 @@ const Fundamentals = (props) => {
     <div className='fundamentals'>
       <div className='fundamentals__container'>
         <div className='fundamentals__label'>
-          Market Cap <Tooltip title={<span className='tooltip'>"Market Capitalization is the total value of all of the company's shares of stock. You can calculate market cap by multiplying the Share Price with the number of Shares Outstanding."</span>} arrow><a href='https://www.investopedia.com/terms/m/marketcapitalization.asp'><HelpOutlineIcon fontSize={'inherit'} /></a></Tooltip>
+          Market Cap 
+          <Tooltip title={<span className='tooltip'>{indicatorDescriptions.marketCap.desc}</span>} arrow>
+            <a href={indicatorDescriptions.marketCap.link}><HelpOutlineIcon fontSize={'inherit'} /></a>
+          </Tooltip>
         </div>
         <div className='fundamentals__value'>{ marketCap }</div>
       </div>
       <div className='fundamentals__container'>
         <div className='fundamentals__label'>
-          Beta <Tooltip title={<span className='tooltip'>'Beta is a measure a market risk compared to the overall market. A stock with Beta greater than 1 indicates a stock more volatile than that of the overall market, and a stock with Beta less than 1 indicates a stock less volatile. A company with a negative Beta tends to move in the opposite direction of the overall stock market'</span>} arrow><a href='https://www.investopedia.com/terms/b/beta.asp'><HelpOutlineIcon fontSize={'inherit'} /></a></Tooltip>
+          Beta 
+          <Tooltip title={<span className='tooltip'>{indicatorDescriptions.beta.desc}</span>} arrow>
+            <a href={indicatorDescriptions.beta.link}><HelpOutlineIcon fontSize={'inherit'} /></a>
+          </Tooltip>
         </div>
         <div className='fundamentals__value'>{ props.beta }</div>
       </div>
       <div className='fundamentals__container'>
         <div className='fundamentals__label'>
-          Avg Volume <Tooltip title={<span className='tooltip'>'Average Volume is the average number of shares of this company traded per day'</span>} arrow><a href='https://www.investopedia.com/terms/a/averagedailytradingvolume.asp'><HelpOutlineIcon fontSize={'inherit'} /></a></Tooltip>
+          Avg Volume 
+          <Tooltip title={<span className='tooltip'>{indicatorDescriptions.avgVol.desc}</span>} arrow>
+            <a href={indicatorDescriptions.avgVol.link}><HelpOutlineIcon fontSize={'inherit'} /></a>
+          </Tooltip>
         </div>
         <div className='fundamentals__value'>{ avgVolume }</div>
       </div>
       { props.lastDiv !== 0 &&
       <div className='fundamentals__container'>
         <div className='fundamentals__label'>
-          Last Dividend <Tooltip title={<span className='tooltip'>'The value of the last dividend paid out by the company'</span>} arrow><a href='https://www.investopedia.com/terms/d/dividend.asp'><HelpOutlineIcon fontSize={'inherit'} /></a></Tooltip>
+          Last Dividend 
+          <Tooltip title={<span className='tooltip'>{indicatorDescriptions.lastDiv.desc}</span>} arrow>
+            <a href={indicatorDescriptions.lastDiv.link}><HelpOutlineIcon fontSize={'inherit'} /></a>
+          </Tooltip>
         </div>
         <div className='fundamentals__value'>${ props.lastDiv } per share</div>
       </div>
---
+++ src/variables/indicatorDescriptions.js
@@ -0,0 +1,20 @@
+const indicatorDescriptions = {
+  marketCap: {
+    link: 'https://www.investopedia.com/terms/m/marketcapitalization.asp',
+    desc: "Market Capitalization is the total value of all of the company's shares of stock. Market Cap can be calculated by multiplying the Share Price with the number of Shares Outstanding."
+  },
+  beta: {
+    link: 'https://www.investopedia.com/terms/m/marketcapitalization.asp',
+    desc: 'Beta is a measure of market risk compared to the overall stock market. A stock with Beta greater than 1 indicates it is more volatile than the overall market, and a stock with Beta less than 1 indicates it is less volatile. A company with a negative Beta tends to move in the opposite direction of the overall market.'
+  },
+  avgVol: {
+    link: 'https://www.investopedia.com/terms/a/averagedailytradingvolume.asp',
+    desc: 'Average Volume is the average number of shares of this company traded per day.'
+  },
+  lastDiv: {
+    link: 'https://www.investopedia.com/terms/d/dividend.asp',
+    desc: 'The value of the last dividend paid out by the company.'
+  }
+}
+
+export { indicatorDescriptions }
